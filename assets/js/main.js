bf6c7e2e47ec380c501eb860e6c6c890a7dea07c
RafaelBamberg <raafaelbamberg@gmail.com>
2021-11-02 13:46:04 -0300

    rmv pjct

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,6 +95,6 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
-sr.reveal(`.category__data, .trick__content, .footer__content`,{interval: 100})
+sr.reveal(`.category__data, .base__content, .footer__content`,{interval: 100})
 sr.reveal(`.about__data, .discount__img`,{origin: 'left'})
 sr.reveal(`.about__img, .discount__data`,{origin: 'right'})
